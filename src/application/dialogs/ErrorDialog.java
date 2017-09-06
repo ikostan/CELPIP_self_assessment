5d5a58d44a6e9769c6b6b23b582de9f6ca6da9e7
ikostan <superegorius@gmail.com>
2017-09-06 14:54:47 -0700

    Error and Exception

--- src/application/dialogs/ErrorDialog.java
+++ src/application/dialogs/ErrorDialog.java
@@ -1,5 +1,7 @@
 package application.dialogs;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
 *
@@ -8,5 +10,23 @@ package application.dialogs;
 
 public class ErrorDialog {
 
+	private Alert alert;
+	private final String TITLE = "Error Dialog";
+	private final String headerTxt;
+	private final String errorTxt;
+	
+	public ErrorDialog(String headerTxt, String errorTxt){
+		
+		this.headerTxt = headerTxt;
+		this.errorTxt = errorTxt;
+	}
+	
+	public void showError(){
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setTitle(TITLE);
+		alert.setHeaderText(headerTxt);
+		alert.setContentText(errorTxt);
+	}
 	
 }
--- src/application/dialogs/ExceptionDialog.java
+++ src/application/dialogs/ExceptionDialog.java
@@ -1,5 +1,11 @@
 package application.dialogs;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 /**
 *
@@ -7,5 +13,63 @@ package application.dialogs;
 */
 
 public class ExceptionDialog {
+	
+	private Alert alert;
+	private final String TITLE = "Exception Dialog";
+	private String headerTxt;
+	private String errorTxt;
+	private String exceptionText;
+	
+	private Label label;
+	private TextArea textArea;
+	private GridPane expContent;
+	
+	
+	public ExceptionDialog(String headerTxt, String errorTxt, String exceptionText){
+		
+		 this.headerTxt = headerTxt;
+		 this.errorTxt = errorTxt;
+		 this.exceptionText = exceptionText;
+		 
+		 label = new Label("The exception stacktrace was:");
+		 textArea = new TextArea(exceptionText);
+		 expContent = new GridPane();
+	}
+	
+	public void showException(){
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setTitle(TITLE);
+		alert.setHeaderText(headerTxt);
+		alert.setContentText(errorTxt);
+		
+		setExceptionMessage();
+		
+		// Set expandable Exception into the dialog pane.
+		alert.getDialogPane().setExpandableContent(expContent);
+		alert.showAndWait();
+	}
+
+	// Create expandable Exception.
+	//StringWriter sw = new StringWriter();
+	//PrintWriter pw = new PrintWriter(sw);
+	//ex.printStackTrace(pw);
+	//String exceptionText = sw.toString();
+
+	
+	private void setExceptionMessage(){
+		
+		textArea.setEditable(false);
+		textArea.setWrapText(true);
+		textArea.setMaxWidth(Double.MAX_VALUE);
+		textArea.setMaxHeight(Double.MAX_VALUE);
+		
+		GridPane.setVgrow(textArea, Priority.ALWAYS);
+		GridPane.setHgrow(textArea, Priority.ALWAYS);
+		
+		expContent.setMaxWidth(Double.MAX_VALUE);
+		expContent.add(label, 0, 0);
+		expContent.add(textArea, 0, 1);
+	}
 
 }
